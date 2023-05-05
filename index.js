36a00094eedfe281c333ac9bf94604ab30af6c97
Stefan Krüger <stefan-krueger@hotmail.de>
2023-05-05 14:58:53 +0200

    add media

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
+
 app.listen(process.env.PORT || 3000)
