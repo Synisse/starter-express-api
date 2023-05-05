bf3388ca67ae6bc4a2aabd42647e98c3e3d1c687
Stefan Krüger <stefan-krueger@hotmail.de>
2023-05-05 15:01:30 +0200

    fix import

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 app.all('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
