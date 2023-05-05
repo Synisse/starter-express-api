be374d2cea6285c021203d55f2befa622e507fe0
Stefan Krüger <stefan-krueger@hotmail.de>
2023-05-05 15:05:59 +0200

    fix express

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.all('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
-
+// app.all('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// })
+app.use(express.static('public'));
 app.listen(process.env.PORT || 3000)
